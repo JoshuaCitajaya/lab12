28c8a0a84eb1b9ec42ff4364d15fee029c9325f1
Joshua Citajaya <josh.citajaya@gmail.com>
2022-04-29 12:25:45 -0700

    yeet1

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -32,7 +32,9 @@ public class FamilyTree
         void addChild(TreeNode childNode)
         {
             // Add childNode to this node's children list. Also
+        	children.add(childNode);
             // set childNode's parent to this node.
+    		childNode.parent = parent;
         }
         
         
@@ -41,14 +43,18 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
-                return this;
+            if ((this.name).equals(targetName)) {
+            	return this;
+            }
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+            	if (child.getNodeWithName(targetName) != null) {
+            		// then that's the node we're looking for. Return it.
+            		return child;
+            	}
             }
             
             // Not found anywhere.
@@ -63,6 +69,11 @@ public class FamilyTree
             ArrayList<TreeNode> ancestors = new ArrayList<>();
 
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
+            TreeNode current = this.parent;
+            while (current != null) {
+            	ancestors.add(current);
+            	current = current.parent;
+            }
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
